2f4b6f5499b3edfc90574c30869a63edb4421109
mursidx <iammursid@gmail.com>
2024-10-25 18:59:53 +0530

    first commit

--- app.js
+++ app.js
@@ -68,5 +68,11 @@ app.set('view engine', 'ejs')
 
 app.use('/', indexRouter);
 
-server.listen(3000)
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
